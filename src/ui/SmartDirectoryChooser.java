6a272de8f3d9f7d426ed68b613500f775849716b
getting.xi <getting.xi@foxmail.com>
2016-04-23 13:34:22 +0800

    修改SmartDirectoryChooser的逻辑

--- src/ui/SmartDirectoryChooser.java
+++ src/ui/SmartDirectoryChooser.java
@@ -1,5 +1,7 @@
 package ui;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Window;
 
@@ -14,8 +16,19 @@ public class SmartDirectoryChooser {
         File directory = directoryChooser.showDialog(ownerWindow);
         if (directory != null) {
             DirectoryRecord.set(getClass(), directory);
+            lastDirectory.set(directory);
         }
         return directory;
     }
+
+    public SmartDirectoryChooser() {
+        lastDirectory.set(DirectoryRecord.get(getClass()));
+    }
+
+    public ObjectProperty<File> lastDirectoryProperty() {
+        return lastDirectory;
+    }
+
+    private ObjectProperty<File> lastDirectory = new SimpleObjectProperty<>();
 
 }
